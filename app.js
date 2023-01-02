533534be8cf6615746e5043fd7f0d407117f2859
Kiet Dang <kdgdang@ucdavis.edu>
2023-01-01 23:32:59 -0800

    cleaned up code and fixed color for types

--- app.js
+++ app.js
@@ -4,22 +4,28 @@ const submit=document.querySelector('input[type=submit]');
 const showPokemon=document.querySelector('.body-pokemon');
 const form=document.querySelector('form');
 const questionMark=document.querySelector('.question-mark');
-
-console.log(questionMark)
-
- form.addEventListener('submit',pokemon)
+const random=document.querySelector('#random');
 
 
-   
-async function pokemon(e){
+
+form.addEventListener('submit',function(e){
 e.preventDefault();
+pokemon(input.value)
+})
+
+random.addEventListener('click',function(){
+  let randomNumber=Math.floor(Math.random()*900);
+  pokemon(randomNumber);
+})
 
 
-if(input.value.length===0){
+async function pokemon(value){
 
-}
-
-    const description=await fetch(`https://pokeapi.co/api/v2/pokemon/${input.value.toLowerCase()}`);
+let dataType=value
+  if(typeof value==='string'){
+dataType=value.toLowerCase();
+  }
+    const description=await fetch(`https://pokeapi.co/api/v2/pokemon/${dataType}`);
     const data=await description.json();
 
     const types=[];
@@ -28,7 +34,6 @@ for(let i=0;i<data.types.length;i++){
 
     types.push(data.types[i].type.name)
 }
-
 
 showPokemon.innerHTML=`
 <div class='container'>
@@ -44,16 +49,14 @@ showPokemon.innerHTML=`
       </div>
       <div class='types'>
         <h2>
-          Type:
-          <span>${types.map((x)=>{return x + '';})}</span> 
+          Type: 
         </h2>
+        <span id='color-types'>${types[0]}</span>
       </div>
       <div class='hp'>
       <h2>
       HP:
-
-      <h3><span>${data.stats[0].base_stat}</span> </h3>
-
+      <h3><span>${data.stats[0].base_stat}</span></h3>
       </h2>
       </div>
       <div class='height'>
@@ -62,8 +65,6 @@ showPokemon.innerHTML=`
       <h3>${data.height} ft</h3>
       </h2>
       </div>
-
-
       <div class='weight lb'>
       <h2>
       Weight:
@@ -74,7 +75,8 @@ showPokemon.innerHTML=`
       `;
 
 input.value='';
-console.log(data.sprites)
+
+//Toggle Pokemon front and back
 const container=document.querySelector('.container');
 
   let switching=true;
@@ -90,98 +92,79 @@ container.children[0].addEventListener('mouseenter',function(){
   }
   
 })
+
+//append new span
+const typesContainer=document.querySelector('.types');
+let type2=''
+if(types.length>1){
+const newSpan=document.createElement("span");
+newSpan.setAttribute('id','color-types1');
+newSpan.innerHTML=`${types[1]}`
+typesContainer.appendChild(newSpan);
+type2=document.querySelector('#color-types1');
+}
+
+//Changing color of Types
+const colorTypes=document.querySelector('#color-types');
+function colorsOfPokemon(color,span){
+  switch(color){
+     case 'normal':
+span.setAttribute('style','color:white');
+break;
+    case 'fire':
+span.setAttribute('style','color:red');
+break;
+ case 'water':
+span.setAttribute('style','color:blue');
+break;
+ case 'grass':
+span.setAttribute('style','color:green');
+break;
+ case 'electric':
+span.setAttribute('style','color:yellow');
+break;
+ case 'ice':
+span.setAttribute('style','color:rgb(135,206,250)');
+break;
+case 'fighting':
+span.setAttribute('style','color:#FF8C00');
+break;
+case 'ghost':
+span.setAttribute('style','color:purple');
+break;
+case 'poison':
+span.setAttribute('style','color:rgb(153,50,204)');
+break;
+case 'ground':
+span.setAttribute('style','color:#654321');
+break;
+case 'psychic':
+span.setAttribute('style','color:rgb(221,160,221)');
+break;
+case 'bug':
+span.setAttribute('style','color:#66FF00');
+break;
+case 'rock':
+span.setAttribute('style','color:#707070');
+break;
+case 'dragon':
+span.setAttribute('style','color:	rgb(204,204,0)');
+break;
+case 'dark':
+span.setAttribute('style','color:	black');
+break;
+case 'steel':
+span.setAttribute('style','color:	#43464B');
+break;
+case 'fairy':
+span.setAttribute('style','color:	pink');
+break;
+  }
+}
+colorsOfPokemon(types[0],colorTypes);
+colorsOfPokemon(types[1],type2);
 };
 
-
-const random=document.querySelector('#random');
-
-random.addEventListener('click',pokemonRandom)
-
-async function pokemonRandom(e){
-  let randomNumber=Math.floor(Math.random()*900);
-    const description=await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
-    const data=await description.json();
-
-        const types1=[];
-    
-for(let x=0;x<data.types.length;x++){
-
-    types1.push(data.types[x].type.name)
-}
-
-
-showPokemon.innerHTML=`
-<div class='container'>
-        <img
-        src=${data.sprites.front_default}
-        alt=""
-        id="pokemon-picture"
-      />
-      <div class='names'>
-        <h2>
-          Name: <span>${data.species.name}</span>
-        </h2>
-      </div>
-      <div class='types'>
-        <h2>
-          Type:
-          <span>${types1.map((x)=>{return x + '';})}</span> 
-        </h2>
-      </div>
-      <div class='hp'>
-      <h2>
-      HP:
-
-      <h3><span>${data.stats[0].base_stat}</span> </h3>
-
-      </h2>
-      </div>
-      <div class='height'>
-      <h2>
-      Height:
-      <h3>${data.height} ft</h3>
-      </h2>
-      </div>
-
-
-      <div class='weight lb'>
-      <h2>
-      Weight:
-      <h3>${data.weight} lb</h3>
-      </h2>
-      </div>
-      </div>
-      `;
-    console.log(data);
-    const container=document.querySelector('.container');
-
-  let switching=true;
-container.children[0].addEventListener('mouseenter',function(){
-
-  if(switching==true){
-    container.children[0].setAttribute('src',data.sprites.back_default);
-    switching=false;
-  }
-  else if(switching==false){
-    container.children[0].setAttribute('src',data.sprites.front_default);
-  switching=true;
-  }
-  
-})
-}
-
-
-
-
-
-/*async function pokemon(e){
-e.preventDefault();
-    const description=await fetch(`https://pokeapi.co/api/v2/pokemon/${input}`);
-    const data=await description.json();
-    event.preventDefault();
-console.log(data)
-}
-pokemon()*/
 
 
 
